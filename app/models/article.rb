fa89d294f4a372059aa9d132307b51028c6b9945
Rails Vinyl Swap Shop <anthealy@gmail.com>
2018-07-01 17:56:40 +0000

    create new template, action and route

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,8 @@
 class Article < ApplicationRecord
-  validates :artist, presence: true  #enforces an artist entry into Article database
-  validates :album, presence: true  #enforces an album entry into Article database
-  
+  #enforces an artist entry into Article database
+  validates :artist, presence: true, length: { minimum: 3, maximum: 50 }
+  #enforces an album entry into Article database
+  validates :album, presence: true
+  #enforces an description entry into Article database, with minimum 20, max 300 charater entry 
+  validates :description, presence: true, length: { minimum: 20, maximum: 300 } 
 end
