d9f43b9169de51abce9369940014c11412ec8c5a
Rails Vinyl Swap Shop <anthealy@gmail.com>
2018-07-04 05:16:46 +0000

    Extract redundancy and prep for heruku

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
+  
+  
   def index
     @articles = Article.all
   end
@@ -8,27 +11,22 @@ class ArticlesController < ApplicationController
   end
   
   def edit
-    @article = Article.find(params[:id])
+    
   end
   
   def create
 
     @article = Article.new(article_params)
-
     if @article.save
-
-     flash[:notice] = "Article was successfully created"
-
-      redirect_to article_path(@article)
-
-      else
-
+     flash[:notice] = "Article was successfully created"   
+     redirect_to article_path(@article)
+    else
       render 'new'
     end
   end
   
   def update
-    @article = Article.find(params[:id])
+    
     if @article.update(article_params)
       flash[:notice] = "Item was updated"
       redirect_to article_path(@article)
@@ -39,12 +37,12 @@ class ArticlesController < ApplicationController
   
   def show
 
-  @article = Article.find(params[:id])
+  
 
   end
   
   def destroy
-    @article = Article.find(params[:id])
+    
     @article.destroy
     flash[:notice] = "Item successfully deleted"
     redirect_to articles_path
@@ -52,6 +50,9 @@ class ArticlesController < ApplicationController
   
   
   private
+   def set_article
+     @article = Article.find(params[:id])
+   end
   def article_params
     params.require(:article).permit(:artist, :album, :description)
   end
