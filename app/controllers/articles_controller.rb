9f56be15af3478d809c4ff418c701efc2e922315
Rails Vinyl Swap Shop <anthealy@gmail.com>
2018-07-05 19:14:20 +0000

    style errors and messages oartials

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,7 +18,7 @@ class ArticlesController < ApplicationController
 
     @article = Article.new(article_params)
     if @article.save
-     flash[:notice] = "Article was successfully created"   
+     flash[:success] = "Article was successfully created"   
      redirect_to article_path(@article)
     else
       render 'new'
@@ -28,7 +28,7 @@ class ArticlesController < ApplicationController
   def update
     
     if @article.update(article_params)
-      flash[:notice] = "Item was updated"
+      flash[:success] = "Item was updated"
       redirect_to article_path(@article)
     else
       render 'edit'
@@ -44,7 +44,7 @@ class ArticlesController < ApplicationController
   def destroy
     
     @article.destroy
-    flash[:notice] = "Item successfully deleted"
+    flash[:danger] = "Item successfully deleted"
     redirect_to articles_path
   end
   
