e1447df37865f13157421a0df4720e100baa46d8
Rails Vinyl Swap Shop <anthealy@gmail.com>
2018-07-08 14:02:59 +0000

    Add secure password and attribute to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180708104941) do
+ActiveRecord::Schema.define(version: 20180708135706) do
 
   create_table "articles", force: :cascade do |t|
     t.string "artist"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20180708104941) do
     t.string "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
