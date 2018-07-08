ec9ae0fd94290080c4a5e86811892aebb11b1f73
Rails Vinyl Swap Shop <anthealy@gmail.com>
2018-07-08 10:33:09 +0000

    Add user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ApplicationRecord
+   #ensures a username is present, is unique, case sensitive and 
+  validates :username, presence: true, uniqueness: {case_sensitive: false},
+              #has a min and max length. 
+              length: { minimum: 3, maximum: 25} 
+   #sets email format  restrictions
+  VALID_EMAIL_REGEX= /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+   #ensures an email address is present, is unique, case 
+   #sensitive, has a min and max length, and is verified. 
+  validates :email, presence: true, length: { maximum: 105 },
+            uniqueness: { case_sensitive: false },
+            format: { with: VALID_EMAIL_REGEX }
+             
   
 end
