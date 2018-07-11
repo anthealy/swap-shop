fa28f20d29534d799ad64a7c8b2778003a4e2f38
Rails Vinyl Swap Shop <anthealy@gmail.com>
2018-07-11 07:02:03 +0000

    Create show action and add gravatar image

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,12 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
   
+  def show
+    @user = User.find(params[:id]) #find user based on is that's presented
+    
+  end
+  
+  
   def update
     @user = User.find(params[:id]) #find user
     if @user.update(user_params) #allows edit
