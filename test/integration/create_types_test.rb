988cc40eeb2147693dd19c90373590ab49c6135c
Rails Vinyl Swap Shop <anthealy@gmail.com>
2018-07-31 20:14:35 +0000

    added email admin and rules links

--- test/integration/create_types_test.rb
+++ test/integration/create_types_test.rb
@@ -9,14 +9,14 @@ class CreateTypeTest < ActionDispatch::IntegrationTest
   
   test "get new type form and create type" do
     sign_in_as(@user, "password") 
-    get new_type_path
-    assert_template 'types/new'
-    assert_difference 'Type.count', 1 do
+    get new_type_path         #creates a new type path
+    assert_template 'types/new'    #asserts the types/new template
+    assert_difference 'Type.count', 1 do    #there should be a difference count of one after this action
       post types_path, params:{type:{name: "LP"}} 
-      follow_redirect!
+      follow_redirect!  #posts http post request to types/path
       end
-      assert_template 'types/index'
-      assert_match "LP", response.body
+      assert_template 'types/index'  #then returns to index
+      assert_match "LP", response.body  # displays LP 
   end
   
    test "invalid type submission results in failure" do
